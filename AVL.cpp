46dc6dce7a24ef4732f8ab5b1ab56ba6919bb317
Mauricio Quintero <Mauricio.Quintero@fi365.ort.edu.uy>
2025-03-13 19:34:25 -0300

    Implementados métodos de rotación y imprimir AVL ordenado

--- AVL.cpp
+++ AVL.cpp
@@ -1,91 +1,147 @@
 #include <cassert>
+#include <iostream>
 
 using namespace std;
-template <typename T> struct AVLNode {
-  T data;
-  int height;
-  AVLNode *left;
-  AVLNode *right;
-  AVLNode(T data) {
-    this->data = data;
-    this->left = nullptr;
-    this->right = nullptr;
-    this->height = 1;
-  }
+template <typename T>
+struct AVLNode
+{
+	T data;
+	int height;
+	AVLNode *left;
+	AVLNode *right;
+	AVLNode(T data)
+	{
+		this->data = data;
+		this->left = nullptr;
+		this->right = nullptr;
+		this->height = 1;
+	}
 };
 int max(int a, int b) { return a > b ? a : b; }
-template <typename T> class AVL {
+template <typename T>
+class AVL
+{
 private:
-  AVLNode<T> *root;
-  int getHeight(AVLNode<T> *node) {
-    if (node == nullptr) {
-      return 0;
-    }
-    return node->height;
-  }
-  void updateHeight(AVLNode<T> *node) {
+	AVLNode<T> *root;
+	int getHeight(AVLNode<T> *node)
+	{
+		if (node == nullptr)
+		{
+			return 0;
+		}
+		return node->height;
+	}
+	void updateHeight(AVLNode<T> *node)
+	{
 
-    int leftHeight = getHeight(node->left);
-    int rightHeight = getHeight(node->right);
-    node->height = 1 + max(leftHeight, rightHeight);
-  }
-  int getBalance(AVLNode<T> *node) {
-    if (node == nullptr) {
-      return 0;
-    }
-    return getHeight(node->right) - getHeight(node->left);
-  }
-  void insertRec(AVLNode<T> *&node, T data) {
-    if (node == nullptr) {
-      node = new AVLNode<T>(data);
-    }
-    if (data < node->data) {
-      this->insertRec(node->left, data);
-    } else {
-      this->insertRec(node->right, data);
-    }
-    this->updateHeight(node);
+		int leftHeight = getHeight(node->left);
+		int rightHeight = getHeight(node->right);
+		node->height = 1 + max(leftHeight, rightHeight);
+	}
+	int getBalance(AVLNode<T> *node)
+	{
+		if (node == nullptr)
+		{
+			return 0;
+		}
+		return getHeight(node->right) - getHeight(node->left);
+	}
+	void insertRec(AVLNode<T> *&node, T data)
+	{
+		if (node == nullptr)
+		{
+			node = new AVLNode<T>(data);
+		}
+		else if (data < node->data)
+		{
+			this->insertRec(node->left, data);
+		}
+		else
+		{
+			this->insertRec(node->right, data);
+		}
+		this->updateHeight(node);
 
-    int balance = getBalance(node);
+		int balance = getBalance(node);
 
-    if (balance <= -2 && node->left->data > data) {
-      // left left case
-      this->rightRotate(node);
-    }
+		if (balance <= -2 && node->left->data > data)
+		{
+			// left left case ->
+			this->rightRotate(node);
+		}
 
-    if (balance <= -2 && node->left->data < data) {
-      // left right case
-      this->leftRotate(node->left);
-      this->rightRotate(node);
-    }
+		if (balance <= -2 && node->left->data < data)
+		{
+			// left right case
+			this->leftRotate(node->left);
+			this->rightRotate(node);
+		}
 
-    if (balance >= 2 && node->right->data < data) {
-      // right right case
-      this->leftRotate(node);
-    }
+		if (balance >= 2 && node->right->data < data)
+		{
+			// right right case
+			this->leftRotate(node);
+		}
 
-    if (balance >= 2 && node->right->data > data) {
-      // right left case
-      this->rightRotate(node->right);
-      this->leftRotate(node);
-    }
-  }
-  bool existRec(AVLNode<T> *node, T data) {
-    if (node == nullptr) {
-      return false;
-    }
-    if (node->data == data) {
-      return true;
-    }
-    if (data < node->data) {
-      return this->existRec(node->left, data);
-    } else {
-      return this->existRec(node->right, data);
-    }
-  }
+		if (balance >= 2 && node->right->data > data)
+		{
+			// right left case
+			this->rightRotate(node->right);
+			this->leftRotate(node);
+		}
+	}
+	bool existRec(AVLNode<T> *node, T data)
+	{
+		if (node == nullptr)
+		{
+			return false;
+		}
+		if (node->data == data)
+		{
+			return true;
+		}
+		if (data < node->data)
+		{
+			return this->existRec(node->left, data);
+		}
+		else
+		{
+			return this->existRec(node->right, data);
+		}
+	}
+	void rightRotate(AVLNode<T> *&node)
+	{
+		AVLNode<T> *a = node->left;
+		AVLNode<T> *b = node;
+		AVLNode<T> *t2 = a->right;
+		node = a;
+		a->right = b;
+		b->left = t2;
+		this->updateHeight(b);
+		this->updateHeight(a);
+	}
+	void leftRotate(AVLNode<T> *&node)
+	{
+		AVLNode<T> *a = node;
+		AVLNode<T> *b = node->right;
+		AVLNode<T> *t2 = b->left;
+		node = b;
+		a->right = t2;
+		b->left = a;
+		this->updateHeight(a);
+		this->updateHeight(b);
+	}
+	void printInOrderRec(AVLNode<T>* node){
+		if(node==nullptr)
+			return;
+		printInOrderRec(node->left);
+		cout << node->data << endl;
+		printInOrderRec(node->right);		
+	}
 
 public:
-  AVL() { this->root = nullptr; }
-  void insert(T data) { this->insertRec(this->root, data); }
-  bool exist(T data) { return this->existRec(this->root, data); }
+	AVL() { this->root = nullptr; }
+	void insert(T data) { this->insertRec(this->root, data); }
+	bool exist(T data) { return this->existRec(this->root, data); }
+	void printInOrder() { printInOrderRec(this->root); }
 };
--- main.cpp
+++ main.cpp
@@ -6,5 +6,11 @@
 using namespace std;
 int main() {
   AVL<int> *miArbolito = new AVL<int>();
+  miArbolito->insert(2);
+  miArbolito->insert(3);
+  miArbolito->insert(4);
+  miArbolito->insert(1);
+  miArbolito->insert(-3);
+  miArbolito->printInOrder();
   return 0;
 }
